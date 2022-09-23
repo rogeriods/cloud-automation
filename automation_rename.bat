b6818c393022e7022db22fa9ad6bb7cca7a3d672
Rogério Domingos <rogerio.ds@hotmail.com>
2022-09-23 13:33:23 -0300

    added clean up files on bat

--- automation_rename.bat
+++ automation_rename.bat
@@ -4,5 +4,14 @@ xcopy /s/z/y <source> dumps\<destination>
 REM Compact the dumps folder 
 zip -r dumps.zip dumps
 
+REM Delete folders and files
+rmdir /s/q  <source> dumps\<destination>
+
+del /s/q <source> dumps\<destination>
+
+REM Build and running the application
 dotnet build
 dotnet run
+
+REM Delete dumps.zip file
+del /s/q dumps.zip
